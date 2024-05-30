a89defcaff20af7c3fa2aca666aa45497a282225
chounaaaa <sunavij2@gmail.com>
2024-05-30 17:39:43 -0300

    agrego menu

---
+++ Menu/Menu/MenuPrincipal.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Menu
+{
+    class MenuPrincipal
+    {
+        public List<Menu> menu = new List<Menu>();
+        int numeroMenu = 0;
+
+
+        public MenuPrincipal(Dictionary<string, string[]> menus)
+        {
+            int fila = 1;
+            int col = 1;
+            // Recorro el diccionario
+            //int pos = 1;
+            foreach (var subMenu in menus)
+            {
+                menu.Add(new Menu(0, subMenu.Key, subMenu.Value, fila, col));
+                col += subMenu.Key.Length + 2;
+            }
+
+        }
+
+
+
+        public void dibujar()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+
+            foreach (var c in menu)
+            {
+                Console.SetCursorPosition(c.columna, c.fila++);
+                if (c == menu[numeroMenu])
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(c.nombreMenu);
+                    for (int i = 0; i < c.items.Length; i++)
+                    {
+                        Console.SetCursorPosition(c.columna, c.fila++);
+                        if (c.items[i] == c.items[c.posItem])
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red; // Cambia el color de fondo a verde
+                            Console.ForegroundColor = ConsoleColor.Black; // Cambia el color del texto a negro
+                            Console.WriteLine(c.items[i]);
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(c.items[i]);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.WriteLine(c.nombreMenu);
+                    Console.SetCursorPosition(0, c.items.Length + 1);
+                }
+                c.fila = 1;
+            }
+        }
+        public void arriba()
+        {
+
+            menu[numeroMenu].posItem--;
+
+            if (menu[numeroMenu].posItem < 0)
+                menu[numeroMenu].posItem = menu[numeroMenu].items.Length - 1;
+            dibujar();
+        }
+
+        public void abajo()
+        {
+
+            menu[numeroMenu].posItem++;
+
+            if (menu[numeroMenu].posItem >= menu[numeroMenu].items.Length)
+                menu[numeroMenu].posItem = 0;
+            dibujar();
+        }
+
+        public void izq()
+        {
+            numeroMenu--;
+
+            if (numeroMenu < 0)
+                numeroMenu = menu.Count - 1;
+            dibujar();
+        }
+
+        public void der()
+        {
+            numeroMenu++;
+
+            if (numeroMenu >= menu.Count)
+                numeroMenu = 0;
+            dibujar();
+        }
+    }
+}
---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Menu
+{
+    class Item
+    {
+        string opcion;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuPrincipal menu;
+
+            string[] menu1 = { "Nuevo Cliente", "Modificar Cliente", "Listar Clientes", "Salir" };
+            string[] menu2 = { "Nuevo Producto", "Modificar Producto", "Eliminar Producto", "Listar Producto", "Salir." };
+
+            var menus = new Dictionary<string, string[]>
+            {
+                { "Archivo", menu1 }, { "Editar", menu2 },
+            };
+
+            Console.Clear();
+            menu = new MenuPrincipal(menus);
+
+            ConsoleKeyInfo tecla; // Variable para almacenar la tecla presionada
+
+            // Bucle principal del programa que se ejecuta hasta que se presiona la tecla Enter
+            menu.dibujar(); // Dibuja el menú en la posición especificada
+            do
+            {
+                tecla = Console.ReadKey(); // Lee la tecla presionada por el usuario
+
+
+
+
+                // Dependiendo de la tecla presionada, se moverá hacia arriba o abajo en el menú
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        menu.arriba();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        menu.abajo();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear();
+                        menu.izq();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear();
+                        menu.der();
+                        break;
+                }
+            } while (tecla.Key != ConsoleKey.Enter); // Sale del bucle cuando Enter
+
+            Console.ReadKey();
+
+        }
+    }
+}
